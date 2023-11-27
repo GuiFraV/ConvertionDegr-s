c0fa2028dff6e87a37f174fb3d53bff2e5102ba8
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-27 18:05:44 +0100

    refactorisation

--- index.php
+++ index.php
@@ -24,106 +24,94 @@
 
     <?php 
 
+        function celsiusToFahrenheit($celsius){
+            return ($celsius * 9/5) + 32;
+        }
+
+        function celsiusToKelvin($celsius){
+            return $celsius + 273.15;
+        }
+
+        function fahrenheitToCelsius($fahrenheit){
+            return ($fahrenheit - 32) / 1.8;
+        }
+
+        function fahrenheitToKelvin($fahrenheit){
+            return ($fahrenheit + 459.67) * 5/9;
+        }
+
+        function kelvinToCelsius($kelvin) {
+            return $kelvin - 273.15;
+        }
+
+        function kelvinToFahrenheit($kelvin){
+            return (9/5) * ($kelvin - 273.15) + 32;
+        }
+
+
         if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-            $temp = filter_input(INPUT_POST, "temp", FILTER_SANITIZE_NUMBER_FLOAT);
+            $temperature = filter_input(INPUT_POST, "temp", FILTER_SANITIZE_NUMBER_FLOAT);
             $operator1 = htmlspecialchars($_POST["operator1"]);
             $operator2 = htmlspecialchars($_POST["operator2"]);
-
             $error = false;
 
-            if(!$temp){
-                echo "<p>Vous n'avez pas rentré de nombre !</p>";
-                $error = true;
-            }
 
-            if(!is_numeric($temp)){
-                echo "<p>Vous n'avez pas rentré un nombre ou un chiffre</p>";
+            if(!is_numeric($temperature)){
+                echo "<p>Vous n'avez pas rentré un nombre ou un chiffre ! </p>";
                 $error = true;
             }
 
             if(!$error){
-
+                
                 $result = 0;
 
-                if($operator1 === "celsius_temp1" && $operator2 === "celsius"){
-    
-                    echo "<p>La température en Celsius est de ". $temp . "</p>";
-    
-                }
-    
-                if($operator1 === "celsius_temp1" && $operator2 === "fahrenheit"){
-    
-                    $result = ($temp * 9/5) + 32;
-    
-                    echo "<p>La température en Fahrenheit est de ". $result . "</p>";
-    
-                }
-    
-                if($operator1 === "celsius_temp1" && $operator2 === "kelvin"){
-    
-                    $result = $temp + 273.15 ;
-    
-                    echo "<p>La température en Kelvin est de ". $result . "</p>";
-    
-                }
-    
-                if($operator1 === "fahrenheit_temp1" && $operator2 === "celsius"){
-    
-                    $result = ($temp - 32)/1.8;
-    
-                    echo "<p>La température en Celsius est de ". $result . "</p>";
-    
-                }
-    
-                if($operator1 === "fahrenheit_temp1" && $operator2 === "fahrenheit"){
-    
-    
-                    echo "<p>La température en Fahrenheit est de ". $temp . "</p>";
-    
-                }
-    
-                if($operator1 === "fahrenheit_temp1" && $operator2 === "kelvin"){
-    
-                    $result = ($temp + 459.67) * 5/9;
-    
-                    echo "<p>La température en Kelvin est de ". $result . "</p>";
-    
-                }
-    
-                 if($operator1 === "kelvin_temp1" && $operator2 === "celsius"){
-    
-                    $result = $temp - 273.15;
-    
-                    echo "<p>La température en Kelvin est de ". $result . "</p>";
-    
-                }
-    
-                 if($operator1 === "kelvin_temp1" && $operator2 === "fahrenheit"){
-    
-                    $result = (9/5) * ($temp - 273.15) + 32;
-    
-                    echo "<p>La température en Fahrenheit est de ". $result . "</p>";
-    
-                }
-    
-                 if($operator1 === "kelvin_temp1" && $operator2 === "kelvin"){
-    
-                
-                    echo "<p>La température en Kelvin est de ". $temp . "</p>";
-    
+                switch ($operator1) {
+                    case "celsius_temp1":
+                        switch($operator2){
+                            case "celsius":
+                                $result = $temperature;
+                                break;
+                            case "fahrenheit":
+                                $result = celsiusToFahrenheit($temperature);
+                                break;
+                            case "kelvin":
+                                $result = celsiusToKelvin($temperature);
+                                break;
+                        }
+                    break;
+                    case "fahrenheit_temp1":
+                        switch($operator2){
+                            case "celsius":
+                                $result = fahrenheitToCelsius($temperature);
+                                break;
+                            case "fahrenheit":
+                                $result = $temperature;
+                                break;
+                            case "kelvin":
+                                $result = fahrenheitToKelvin($temperature);
+                                break;
+                        }
+                    break;
+                    case "kelvin_temp1":
+                        switch($operator2){
+                            case "celsius":
+                                $result = kelvinToCelsius($temperature);
+                                break;
+                            case "fahrenheit":
+                                $result = kelvinToFahrenheit($temperature);
+                                break;
+                            case "kelvin":
+                                $result =  $temperature;
+                                break;
+                        }
+                    break;
                 }
 
-
+                echo "<p> La température est de " . $result . " ". $operator2 . "</p>";
 
             }
-
-
-
-
-
         }
-    
     ?>
 
     
